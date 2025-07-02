a98a03d15f4ba25248271c768ca99ae8e572e0c3
samclassix <samclassix@proton.me>
2025-07-02 13:53:25 +0200

    feat: RollerV2 overhaul

--- schema/Roller.ts
+++ schema/Roller.ts
@@ -1,14 +1,22 @@
-import { onchainTable } from 'ponder';
+import { onchainTable, primaryKey } from 'ponder';
 
-export const RollerRolled = onchainTable('RollerRolled', (t) => ({
-	id: t.text().primaryKey(),
-	created: t.bigint().notNull(),
-	blockheight: t.bigint().notNull(),
-	owner: t.hex().notNull(),
-	source: t.hex().notNull(),
-	collWithdraw: t.bigint().notNull(),
-	repay: t.bigint().notNull(),
-	target: t.hex().notNull(),
-	collDeposit: t.bigint().notNull(),
-	mint: t.bigint().notNull(),
-}));
+export const RollerRolled = onchainTable(
+	'RollerRolled',
+	(t) => ({
+		created: t.bigint().notNull(),
+		count: t.bigint().notNull(),
+		blockheight: t.bigint().notNull(),
+		owner: t.hex().notNull(),
+		source: t.hex().notNull(),
+		collWithdraw: t.bigint().notNull(),
+		repay: t.bigint().notNull(),
+		target: t.hex().notNull(),
+		collDeposit: t.bigint().notNull(),
+		mint: t.bigint().notNull(),
+	}),
+	(table) => ({
+		pk: primaryKey({
+			columns: [table.source, table.target, table.count],
+		}),
+	})
+);
